dcfecac8161781ace8a85b7dc55fdae01513ab1c
BrunoTeixeira1996 <brunoalexandre3@hotmail.com>
2023-03-16 17:31:01 +0000

    [Logic] movie page done, like validations using jquery

--- utils/handles.go
+++ utils/handles.go
@@ -22,34 +22,61 @@ func IndexHandle(baseTemplate *template.Template) http.HandlerFunc {
 // Handles "/movies"
 func MoviesHandle(baseTemplate *template.Template, db *models.Db) http.HandlerFunc {
 	var movies []models.Movie
+	var title string
 
 	return func(w http.ResponseWriter, r *http.Request) {
-		movieId := r.URL.Query().Get("id")
+		switch r.Method {
+		case "GET":
+			movieId := r.URL.Query().Get("id")
 
-		// List respective movie
-		if movieId != "" {
+			// List respective movie
+			if movieId != "" {
+				if regexp.MustCompile(`\d`).MatchString(movieId) {
+					if err := db.QueryAllFromMovies("select * from movies where id = ?", &movies, movieId); err != nil {
+						fmt.Printf("Error while QueryAllFromMovies for movie id=%s\n", movieId)
+					}
+				}
+
+				title = movies[0].Title
+
+			} else {
+				// List all movies
+				if err := db.QueryAllFromMovies("select * from movies", &movies); err != nil {
+					fmt.Println("Error while handling QueryAllFromMovies")
+				}
+				title = "Movies"
+			}
+
+			page := models.Page{
+				Title: title,
+				Any:   movies,
+			}
+			baseTemplate.Execute(w, page)
+
+			// Since I am using a pointer, I need to clean this slice
+			// or there will be dups
+			movies = nil
+
+		case "POST":
+			// comment := r.FormValue("area_1")
+			// author := r.FormValue("group_1")
+
+			// TODO: insert in database
+
+			movieId := r.URL.Query().Get("id")
 			if regexp.MustCompile(`\d`).MatchString(movieId) {
 				if err := db.QueryAllFromMovies("select * from movies where id = ?", &movies, movieId); err != nil {
 					fmt.Printf("Error while QueryAllFromMovies for movie id=%s\n", movieId)
 				}
 			}
 
-		} else {
-			// List all movies
-			if err := db.QueryAllFromMovies("select * from movies", &movies); err != nil {
-				fmt.Println("Error while handling QueryAllFromMovies")
+			page := models.Page{
+				Title: movies[0].Title,
+				Any:   movies,
 			}
+			baseTemplate.Execute(w, page)
 		}
 
-		page := models.Page{
-			Title: "models.Movies",
-			Any:   movies,
-		}
-		baseTemplate.Execute(w, page)
-
-		// Since I am using a pointer, I need to clean this slice
-		// or there will be dups
-		movies = nil
 	}
 }
 
